c0ae0c3fe9e8783bc4024c6ed67a5a5c6af65747
zzm <zzm@zzms-MacBook-Pro.local>
2015-01-22 00:25:17 +0800

    no message

--- Tutorial4LightMove/main.cpp
+++ Tutorial4LightMove/main.cpp
@@ -13,13 +13,15 @@
 #include "font.h"
 GLfloat lightAmbient[] = {0.0f, 0.0f, 0.0f, 1.0f};
 GLfloat lightDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-GLfloat lightPosition[] = {0.0f, 0.0f, 1.0f, 0.0f};
+GLfloat lightPosition[] = {0.0f, 0.0, 1.0f, 0.0f};
 GLfloat material[]  ={1.0f, 1.0f, 1.0f, 1.0f};
 
 GLuint textureId;
 
 extern int anglex;
 extern int angley;
+extern int lastx;
+extern int lasty;
 void drawCube(){
     glBegin(GL_QUADS);
     // Front Face
@@ -60,37 +62,86 @@ void drawCube(){
     glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
     glEnd();
 }
+void drawCube2(){
+    glBegin(GL_QUADS);
+    // Front Face
+    glNormal3f(-0.577f,-0.577f, 0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    glNormal3f( 0.577f,-0.577f, 0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    glNormal3f( 0.577f, 0.577f, 0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    glNormal3f(-0.577f, 0.577f, 0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    // Back Face
+    glNormal3f(-0.577f,-0.577f,-0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glNormal3f(-0.577f, 0.577f,-0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glNormal3f( 0.577f, 0.577f,-0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    glNormal3f( 0.577f,-0.577f,-0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    // Top Face
+    glNormal3f(-0.577f, 0.577f,-0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glNormal3f(-0.577f, 0.577f, 0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glNormal3f( 0.577f, 0.577f, 0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    glNormal3f( 0.577f, 0.577f,-0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    // Bottom Face
+    glNormal3f(-0.577f,-0.577f,-0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glNormal3f( 0.577f,-0.577f,-0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glNormal3f( 0.577f,-0.577f, 0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    glNormal3f(-0.577f,-0.577f, 0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    // Right face
+    glNormal3f( 0.577f,-0.577f,-0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    glNormal3f( 0.577f, 0.577f,-0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    glNormal3f( 0.577f, 0.577f, 0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    glNormal3f( 0.577f,-0.577f, 0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    // Left Face
+    glNormal3f(-0.577f,-0.577f,-0.577f);glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    glNormal3f(-0.577f,-0.577f, 0.577f);glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    glNormal3f(-0.577f, 0.577f, 0.577f);glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glNormal3f(-0.577f, 0.577f,-0.577f);glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+	glEnd();
+}
+void drawCube(int z){
+    glPushMatrix();
+    glTranslated(-z, 0, 0);
+    
+    glRotatef(anglex, 1, 0, 0);
+    glRotatef(angley, 0, 1, 0);
+    drawCube2();
+    glPopMatrix();
+}
+void drawCube2(int z){
+    glPushMatrix();
+    glTranslated(-z, 0, 0);
+    
+    glRotatef(anglex, 1, 0, 0);
+    glRotatef(angley, 0, 1, 0);
+    drawCube2();
+    glPopMatrix();
+}
 void display(){
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    glBindTexture(GL_TEXTURE_2D, textureId);
+    glLineWidth(6);
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
     glLoadIdentity();
-    glTranslated(0, 0, -15);
-    glPushMatrix();
-    glTranslated(-7, 0, 0);
-    
-    glRotatef(anglex, 1, 0, 0);
-    glRotatef(angley, 0, 1, 0);
-    drawCube();
-    glPopMatrix();
-    glPushMatrix();
-    glTranslated(-3, 0, 0);
-    
-    glRotatef(anglex, 1, 0, 0);
-    glRotatef(angley, 0, 1, 0);
-    drawCube();glPopMatrix();
-    glPushMatrix();
-    glTranslated(2, 0, 0);
-    
-    glRotatef(anglex, 1, 0, 0);
-    glRotatef(angley, 0, 1, 0);
-    drawCube();glPopMatrix();
-    glPushMatrix();
-    glTranslated(6, 0, 0);
-    
-    glRotatef(anglex, 1, 0, 0);
-    glRotatef(angley, 0, 1, 0);
-    drawCube();
-
+    glBegin(GL_LINES);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(-500.0f, 0.0f, 0.0f);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(500.0f, 0.0f, 0.0f);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, -500.0f, 0.0f);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 500.0f, 0.0f);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, -500.0f);
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, 500.0f);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    for (int i = 0; i < 20; i++) {
+        glTranslated(0, 0, -5);
+        drawCube(-6);
+        drawCube2(-3);
+        drawCube(2);
+        drawCube2(5);
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
     glutSwapBuffers();
 }
 int main(int argc, char * argv[])
@@ -106,6 +157,7 @@ int main(int argc, char * argv[])
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0f, 1.0f, 1.0f, 100.0f);
+    gluLookAt(10, 10, 10, 0, 0, 0, 0, 1, 0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     
@@ -113,6 +165,7 @@ int main(int argc, char * argv[])
     glClearDepth(1.0f);
     
     glEnable(GL_TEXTURE_2D);
+    glShadeModel(GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
